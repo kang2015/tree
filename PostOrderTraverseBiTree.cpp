09f77047654a3c1fb3116952b7295105388a817c
AmyKang91 <kangyanli91@outlook.com>
2015-06-17 19:17:12 +0800

    Update PostOrderTraverseBiTree.cpp

--- PostOrderTraverseBiTree.cpp
+++ PostOrderTraverseBiTree.cpp
@@ -1,1 +1,18 @@
-
+    vector<int> postorderTraversal(TreeNode* root) {
+        TreeNode *p = root;
+        stack<TreeNode *> s;
+        vector<int> ans;
+        while(p||!s.empty()){
+            if(p){
+                ans.push_back(p->val);
+                s.push(p);
+                p = p->right;
+            }else{
+                p = s.top();
+                s.pop();
+                p = p->left;
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
