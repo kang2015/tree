e327093486d9681bb1ea79d5b1cb5904f3932cc0
AmyKang91 <kangyanli91@outlook.com>
2015-06-17 15:01:30 +0800

    Update InOrderTraverseBiTree.cpp

--- InOrderTraverseBiTree.cpp
+++ InOrderTraverseBiTree.cpp
@@ -1,5 +1,17 @@
-int InOrderTraverseBiTree(BiNode *root)
+void InOrderTraverseBiTree(BiNode *root)
 {
   if(root == NULL){
+    return;
+  }
+  stack s;
+  s.push(root);
+  while(!s.empty()){
+    BiNode *p = s.top();
+    while(p->left) {s.push(p->left);p=p->left;}
+    s.pop();
+    visit(p);
+    if(p->right!=NULL){
+      s.push(p->right);
+    }
   }
 }
