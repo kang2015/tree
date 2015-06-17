17e760cacdf50f13a05bde23ab3413deea44b94e
AmyKang91 <kangyanli91@outlook.com>
2015-06-17 16:23:20 +0800

    Update InOrderTraverseBiTree.cpp

--- InOrderTraverseBiTree.cpp
+++ InOrderTraverseBiTree.cpp
@@ -1,5 +1,5 @@
 void InOrderTraverseBiTree(BiNode *root){
-     stacjk s; BiNode *p = root;
+     stack s; BiNode *p = root;
      while(p||!s.empty()){
           if(p){s.push(p);p = p->left;}
           else{p = s.top(); visit(p); p.pop(); p= p->right;}
